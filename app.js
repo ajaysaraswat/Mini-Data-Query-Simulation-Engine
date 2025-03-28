f3000816df0d0edfc71b17afb6d693359a8e05ad
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2025-03-28 18:28:54 +0530

    some chg

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
 app.use(express.json());
 app.use(cookieParser());
 
